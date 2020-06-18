8780f32ee52e851fefed6f2465c1a88d2e260ad1
Bindu Khadka <bindu@Rajeshs-MacBook-Pro.local>
2020-06-18 15:51:58 +0545

    updated preview on button pressed

--- lib/main.dart
+++ lib/main.dart
@@ -5,53 +5,81 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(title: 'Calculator'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({this.title});
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String result = "";
 
-  Row buildButton ( {String text1,String text2, String text3, String text4}){
+  void buttonPressed(String value) {
+    setState(() {
+      result += value;
+    });
+  }
+
+  Row buildButton({String text1, String text2, String text3, String text4}) {
     return Row(
       children: <Widget>[
         FlatButton(
-          onPressed: (){},
-          child: Text('${text1}' ,style: TextStyle(color: Colors.white,fontSize: 20.0),),
+          onPressed: () => buttonPressed(text1),
+          child: Text(
+            '$text1',
+            style: TextStyle(color: Colors.blueGrey, fontSize: 20.0),
+          ),
         ),
-        SizedBox(height: 80.0,),
-        FlatButton(
-          onPressed: (){},
-          child: Text('${text2}',style: TextStyle(color: Colors.white,fontSize: 20.0),),
+        SizedBox(
+          height: 80.0,
+          width: 5.0,
         ),
-        SizedBox(height: 80.0,),
         FlatButton(
-          onPressed: (){},
-          child: Text('${text3}',style: TextStyle(color: Colors.white,fontSize: 20.0),),
-
+          onPressed: () {
+            buttonPressed(text2);
+          },
+          child: Text(
+            '$text2',
+            style: TextStyle(color: Colors.blueGrey, fontSize: 20.0),
+          ),
         ),
-        SizedBox(height: 80.0,),
+        SizedBox(
+          height: 80.0,
+          width: 5.0,
+        ),
         FlatButton(
-          onPressed: (){},
-          child: Text('${text4}',style: TextStyle(color: Colors.white,fontSize: 20.0),),
-
+          onPressed: () {
+            buttonPressed(text3);
+          },
+          child: Text(
+            '$text3',
+            style: TextStyle(color: Colors.blueGrey, fontSize: 20.0),
+          ),
+        ),
+        SizedBox(
+          height: 80.0,
+          width: 5.0,
+        ),
+        FlatButton(
+          onPressed: () {
+            buttonPressed(text4);
+          },
+          child: Text(
+            '$text4',
+            style: TextStyle(color: Colors.pink, fontSize: 20.0),
+          ),
         ),
       ],
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,30 +87,55 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Colors.grey[900],
         elevation: 0.0,
-        title: Text(widget.title),
       ),
       body: Column(
         children: <Widget>[
           SizedBox(
-            height: 210,
+            height: 50.0,
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  result,
+                  style: TextStyle(color: Colors.blueGrey, fontSize: 20.0),
+                )),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                'hello',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
           ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
             child: Container(
               height: 1.0,
-              width: 400.0,
-              color: Colors.white,
+              width: 325.0,
+              color: Colors.blueGrey,
             ),
           ),
-          SizedBox(height: 30.0,),
+          SizedBox(
+            height: 30.0,
+          ),
           Column(
             children: <Widget>[
-              buildButton(text1 :'C', text2:'+/-', text3: '%', text4:'/'),
-              buildButton(text1: '7',text2: '8',text3: '9', text4: 'x'),
-              buildButton(text1: '4',text2: '5',text3: '6',text4: '-'),
-              buildButton(text1: '1',text2: '2',text3: '3',text4: '4'),
-              buildButton(text1: '00',text2: '0',text3: '.',text4: '='),
-
+              buildButton(text1: 'C', text2: '+/-', text3: '%', text4: '/'),
+              buildButton(text1: '7', text2: '8', text3: '9', text4: 'x'),
+              buildButton(text1: '4', text2: '5', text3: '6', text4: '-'),
+              buildButton(text1: '1', text2: '2', text3: '3', text4: '+'),
+              buildButton(text1: '00', text2: '0', text3: '.', text4: '='),
             ],
           ),
         ],
